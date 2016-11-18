f33258ae6f86f9c34d96a9c9d5e90eab0fe4867c
Sebastian Guerrero <sguerrer@andrew.cmu.edu>
2016-11-17 19:53:12 -0800

    added some json results

--- src/Common/get_videos.py
+++ src/Common/get_videos.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 
@@ -7,17 +6,22 @@ import urllib2
 import json	
 
 
-def request_video(id, access_token):
+def request_video(pageid, access_token):
 
 	endpoint = 'https://graph.facebook.com/v2.8'
 	#access_token='EAACEdEose0cBAHFPMoobVMkxxWagM9i1SrJY1JKEdLd0xOaT5GqCOLnrFcT4ZA0v0Lr9wxrJKwRCHZBiZApuE7pVjpbIQR6lFJkJCVJ8WdMkfdmr1kPbKgf1GnT0oOvAgMbjP5DGy3Q06W8vSOt87CC8gZCgOLGiqZCoZApxkitQZDZD'
-	fb_graph_url = endpoint+ '/' + 'ShaileneWoodley' + '/videos?fields=live_status,source&access_token='+access_token
+	fb_graph_url = endpoint+ '/' + pageid + '/videos?fields=live_status,source,description,embed_html,id,event,from,icon,length,picture,permalink_url,place,status,title,catpions,comments,likes,reactions,tags,video_insights&access_token='+access_token
 
 
 
 	resp = requests.get(fb_graph_url)
 
-	print json.dumps(resp.json(), sort_keys=True, indent=4, separators=(',',': '))
+	data = json.dumps(resp.json(), sort_keys=True, indent=4, separators=(',',': '))
+
+	with open("result.json", 'w') as f:
+		f.write(data)
+
+	#print json.dumps(resp.json(), sort_keys=True, indent=4, separators=(',',': '))
 
 def main(args):
 
